9e9b839aa7c5cc38e9119eaaf8ef3518da206452
BOUAZAR MEHDI <mbouazar@rc2k.fr>
2021-04-30 16:43:49 +0000

    components creation with UI design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
+import { ArticlesListComponent } from './components/articles-list/articles-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenuListComponent,
+    ArticlesListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
