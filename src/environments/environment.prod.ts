239889b165ddfdade9fde45220c12f05df0dd121
BOUAZAR MEHDI <mbouazar@rc2k.fr>
2021-05-17 15:20:24 +0100

    environment var prod https link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API_BASE_URL : "https://hacker-news.firebaseio.com/v0/",
   API_BASE_URL_SEARCH : "https://hn.algolia.com/api/v1/search_by_date?query=",
-  API_BASE_URL_TAG : "http://hn.algolia.com/api/v1/search_by_date?tags="
+  API_BASE_URL_TAG : "https://hn.algolia.com/api/v1/search_by_date?tags="
 
 };
